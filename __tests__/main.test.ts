c642dd7c82f5e7ce33a20fd58596d3f8db648217
heimanba <yuankun.yk@alibaba-inc.com>
2022-08-17 23:45:27 +0800

    init project

---
+++ __tests__/main.test.ts
@@ -0,0 +1,6 @@
+import {expect, test} from '@jest/globals'
+
+test('demo', () => {
+  const a = 'demo'
+  expect(a).toEqual('demo')
+})
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+/* eslint-disable no-console */
+
+export default function run(): {[key: string]: any} {
+  console.log('run')
+  return {success: true}
+}
